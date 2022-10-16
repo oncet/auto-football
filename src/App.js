791cfa9a7640483831135afbaa8b208a6438e002
oncet <camilorivera86@gmail.com>
2022-10-15 23:53:24 -0300

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="border-8 container mx-auto flex flex-col h-screen">
+      <header className="border">
+        <h1>Auto Football</h1>
       </header>
+      <main>
+        <h2>Tu equipo</h2>
+        <div className="flex">
+          <div className="border flex-1 h-80">
+            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
+              <li className="border">25%</li>
+            </ul>
+          </div>
+          <div className="border flex-1 h-80">
+            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+            </ul>
+          </div>
+          <div className="border flex-1 h-80">
+            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+            </ul>
+          </div>
+          <div className="border flex-1 h-80">
+            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+            </ul>
+          </div>
+        </div>
+        <h2>Tienda</h2>
+        <div className="flex">
+          <div className="border flex-1">
+            <ul className="border border-red-500">
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+            </ul>
+          </div>
+          <div className="border flex-1">
+            <ul className="border border-red-500">
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+            </ul>
+          </div>
+          <div className="border flex-1">
+            <ul className="border border-red-500">
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+              <li className="border">25%</li>
+            </ul>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
