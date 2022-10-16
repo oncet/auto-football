a5bd74d9d25b2e6b573523976085546771abff46
oncet <camilorivera86@gmail.com>
2022-10-16 00:20:02 -0300

    feat: ui changes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,58 @@
 function App() {
   return (
-    <div className="border-8 container mx-auto flex flex-col h-screen">
-      <header className="border">
+    <div className="container mx-auto flex flex-col h-screen gap-4">
+      <header>
         <h1>Auto Football</h1>
       </header>
-      <main>
+      <main className="flex flex-col gap-4">
         <h2>Tu equipo</h2>
-        <div className="flex">
-          <div className="border flex-1 h-80">
-            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
-              <li className="border">25%</li>
+        <div className="flex gap-4">
+          <div className=" flex-1 h-80">
+            <ul className="border flex flex-col items-center h-full justify-center gap-4">
+              <li className="border px-2 py-1">🐵 25%</li>
             </ul>
           </div>
-          <div className="border flex-1 h-80">
-            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
-              <li className="border">25%</li>
-              <li className="border">25%</li>
+          <div className=" flex-1 h-80">
+            <ul className="border flex flex-col items-center h-full justify-center gap-4">
+              <li className="border px-2 py-1">🐮 1-2</li>
+              <li className="border px-2 py-1">🐮 1-2</li>
             </ul>
           </div>
-          <div className="border flex-1 h-80">
-            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
-              <li className="border">25%</li>
-              <li className="border">25%</li>
-              <li className="border">25%</li>
+          <div className=" flex-1 h-80">
+            <ul className="border flex flex-col items-center h-full justify-center gap-4">
+              <li className="border px-2 py-1">🐺 1-2</li>
+              <li className="border px-2 py-1">🐺 1-2</li>
+              <li className="border px-2 py-1">🐺 1-2</li>
             </ul>
           </div>
-          <div className="border flex-1 h-80">
-            <ul className="border border-red-500 flex flex-col items-center h-full justify-center">
-              <li className="border">25%</li>
-              <li className="border">25%</li>
-              <li className="border">25%</li>
-              <li className="border">25%</li>
+          <div className=" flex-1 h-80">
+            <ul className="border flex flex-col items-center h-full justify-center gap-4">
+              <li className="border px-2 py-1">🐯 1-2</li>
+              <li className="border px-2 py-1">🐯 1-2</li>
+              <li className="border px-2 py-1">🐯 1-2</li>
+              <li className="border px-2 py-1">🐯 1-2</li>
             </ul>
           </div>
         </div>
         <h2>Tienda</h2>
-        <div className="flex">
-          <div className="border flex-1">
-            <ul className="border border-red-500">
-              <li className="border">25%</li>
-              <li className="border">25%</li>
+        <div className="flex gap-4">
+          <div className="flex-1">
+            <ul className="flex flex-col gap-4">
+              <li className="border px-2 py-1">🐮 1-3</li>
+              <li className="border px-2 py-1">🐮 2-2</li>
+              <li className="border px-2 py-1">🐮 1-2 c/skill</li>
             </ul>
           </div>
-          <div className="border flex-1">
-            <ul className="border border-red-500">
-              <li className="border">25%</li>
-              <li className="border">25%</li>
-              <li className="border">25%</li>
+          <div className="flex-1">
+            <ul className="flex flex-col gap-4">
+              <li className="border px-2 py-1">🐺 1-3</li>
+              <li className="border px-2 py-1">🐺 2-2</li>
             </ul>
           </div>
-          <div className="border flex-1">
-            <ul className="border border-red-500">
-              <li className="border">25%</li>
-              <li className="border">25%</li>
-              <li className="border">25%</li>
-              <li className="border">25%</li>
+          <div className="flex-1">
+            <ul className="flex flex-col gap-4">
+              <li className="border px-2 py-1">🐯 1-3</li>
+              <li className="border px-2 py-1">🐯 2-2</li>
             </ul>
           </div>
         </div>
