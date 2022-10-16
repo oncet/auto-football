752fc09914e08922e39e7e011daf9952765f9489
Camilo <camilorivera86@gmail.com>
2022-10-16 01:21:44 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 function App() {
   return (
-    <div className="container mx-auto flex flex-col h-screen gap-4 py-4">
+    <div className="container max-w-screen-md mx-auto flex flex-col h-screen gap-4 py-4">
       <header className="font-bold bg-green-400 px-2 py-1 rounded">
         <h1>Auto Football</h1>
       </header>
       <main className="flex flex-col gap-4 ">
         <h2>Tu equipo</h2>
         <div className="flex gap-4">
-          <div className=" flex-1 h-80">
+          <div className=" flex-1 h-60">
             <ul className="bg-green-400 rounded flex flex-col items-center h-full justify-center gap-4">
               <li className="bg-white rounded px-2 py-1">🐵 25%</li>
             </ul>
           </div>
-          <div className=" flex-1 h-80">
+          <div className=" flex-1 h-60">
             <ul className="bg-green-400 rounded flex flex-col items-center h-full justify-center gap-4">
               <li className="bg-white rounded px-2 py-1">🐮 1-2</li>
               <li className="bg-white rounded px-2 py-1">🐮 1-2</li>
             </ul>
           </div>
-          <div className=" flex-1 h-80">
+          <div className=" flex-1 h-60">
             <ul className="bg-green-400 rounded flex flex-col items-center h-full justify-center gap-4">
               <li className="bg-white rounded px-2 py-1">🐺 1-2</li>
               <li className="bg-white rounded px-2 py-1">🐺 1-2</li>
               <li className="bg-white rounded px-2 py-1">🐺 1-2</li>
             </ul>
           </div>
-          <div className=" flex-1 h-80">
+          <div className=" flex-1 h-60">
             <ul className="bg-green-400 rounded flex flex-col items-center h-full justify-center gap-4">
               <li className="bg-white rounded px-2 py-1">🐯 1-2</li>
               <li className="bg-white rounded px-2 py-1">🐯 1-2</li>
