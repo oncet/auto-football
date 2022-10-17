6a080ef89e3914a9d665fdc2313e6e72b7391def
Camilo <camilorivera86@gmail.com>
2022-10-16 23:02:46 -0300

    feat: animationsss

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,10 +1,13 @@
 const ShopItem = ({ player, icon }) => {
   return (
-    <li className="dark:bg-white dark:text-black rounded px-2 py-1">
+    <li className="dark:bg-white dark:text-black rounded px-2 py-1 group">
       <label className="flex justify-between cursor-pointer">
-        <span>
-          {icon} {player}
-        </span>
+        <div className="flex gap-2 group-hover:gap-6 transition-all">
+          <div className="transition-transform group-hover:-rotate-12 group-hover:scale-[2.7]">
+            {icon}
+          </div>{" "}
+          <div>{player}</div>
+        </div>
         <input type="checkbox" name="items" value={icon + 1} />
       </label>
     </li>
